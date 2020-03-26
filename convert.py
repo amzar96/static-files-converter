09cd265d6ecb14e358b0a2283fc28c64f5419dc3
Mohamad Amzar <mohdamzar96@gmail.com>
2020-03-26 13:17:49 +0800

    Fix things

--- convert.py
+++ convert.py
@@ -1,32 +1,41 @@
-from flask import Flask, jsonify, abort, request, make_response, url_for, render_template, redirect, flash
+from flask import (
+    Flask,
+    jsonify,
+    abort,
+    request,
+    make_response,
+    url_for,
+    render_template,
+    redirect,
+    flash,
+)
 import json
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    output="Please Enter path"
+    output = "Please Enter path"
     path = "none"
-    return render_template('form.html', output=output, path=path)
+    return render_template("form.html", output=output, path=path)
 
 
-@app.route('/convert', methods=['POST'])
+@app.route("/convert", methods=["POST"])
 def convert():
-    #path = request.form.get('path')
-    #folder = request.form.get('folder')
+    # path = request.form.get('path')
+    # folder = request.form.get('folder')
 
+    path = request.form["path"]
+    folder = request.form["folder"]
 
-    path =  request.form['path']
-    folder =  request.form['folder']
+    path2 = "{}/{}".format(folder, path)
+    newpath = "{{ url_for('static', filename='" + path2 + "') }}"
 
-    path2 = ("{}/{}".format(folder,path))
-    newpath =  "{{ url_for('static', filename='"+path2+"') }}"
-
-    return json.dumps({'status':'OK','path':path,'newpath':newpath})
+    return json.dumps({"status": "OK", "path": path, "newpath": newpath})
 
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0',port=5050, debug=True)
-    #app.run(host="0.0.0.0", port=constants.API_PORT, debug=True)
-
+    # app.run(host="0.0.0.0", port=5050, debug=True)
+    app.run(debug=True)
+    # app.run(host="0.0.0.0", port=constants.API_PORT, debug=True)
